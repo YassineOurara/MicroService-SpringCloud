3c78b7ff42320712cc85ec054b66106e28d5af99
YassineOurara <yassineourara@gmail.com>
2023-05-28 16:23:45 +0100

    bill

--- gateway-service/src/main/java/orgsid/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/orgsid/gatewayservice/GatewayServiceApplication.java
@@ -25,14 +25,14 @@ public class GatewayServiceApplication {
 	RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route((r) -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
-				.route((r) -> r.path("/products/**").uri("http://PRODUCT-SERVICE"))
+				.route((r) -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
 				.build();
 	}*/
 	/*@Bean
 	RouteLocator routeLocator(RouteLocatorBuilder builder){
 		return builder.routes()
-				.route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
-				.route((r)->r.path("/products/**").uri("http://localhost:8082/"))
+				.route((r)->r.path("/customers/**").uri("http://localhost:1809/"))
+				.route((r)->r.path("/products/**").uri("http://localhost:2023/"))
 				.build();
 	}*/
 
